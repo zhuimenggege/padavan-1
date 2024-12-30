89c07e2e33577d53b70c63024c9797717e3c9499
zhuimenggege <a5622@163.com>
2024-12-31 00:36:29 +0800

    Update dev_init.sh

--- trunk/user/scripts/files/sbin/dev_init.sh
+++ trunk/user/scripts/files/sbin/dev_init.sh
@@ -4,9 +4,9 @@ mount -t proc proc /proc
 mount -t sysfs sysfs /sys
 [ -d /proc/bus/usb ] && mount -t usbfs usbfs /proc/bus/usb
 
-size_tmp="24M"
+size_tmp="120M"
 size_var="4M"
-size_etc="6M"
+size_etc="14M"
 
 if [ "$1" == "-l" ] ; then
 	size_tmp="8M"
